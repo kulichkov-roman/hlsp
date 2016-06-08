f47f6c81f9c5260fe9847718f050f9a45271d816
Roman Kulichkov <roman@kulichkov.pro>
2016-06-08 19:18:13 +0600

    Добавил функцияю getBrandsInDir() для добавления в отфильтрованный смарт фильтра бреднов из конечного раздела каталога

--- bitrix/components/your/catalog.smart.filter/templates/brands/result_modifier.php
+++ bitrix/components/your/catalog.smart.filter/templates/brands/result_modifier.php
@@ -4,6 +4,97 @@ while($item = $resProperty->Fetch()){
 	$arHitPropertyValues[$item["ID"]] = $item;
 }
 
+function getBrandsInDir()
+{
+	global $APPLICATION;
+
+	$environment = \Your\Environment\EnvironmentManager::getInstance();
+
+	$curDir = $APPLICATION->GetCurDir();
+
+	$arUrl = array_unique(explode('/', $curDir));
+	array_splice($arUrl, sizeof($arUrl)-1);
+
+	$arSecSort = array();
+	$arSecFilter = array(
+		"IBLOCK_ID" => $environment->get('catalogIBlock'),
+		"CODE" => $arUrl['3']
+	);
+	$arSecSelect = array(
+		"ID",
+		"NAME",
+	);
+
+	$rsSection =  \CIBlockSection::GetList(
+		$arSecSort,
+		$arSecFilter,
+		false,
+		$arSecSelect,
+		false
+	);
+
+	$arSec = array();
+	if ($arSecItem = $rsSection->Fetch())
+
+	$arElemSort = array();
+	$arElemSelect = array(
+		'ID',
+		'NAME',
+		'PROPERTY_BRAND'
+	);
+
+	$arElemFilter = array(
+		'IBLOCK_ID' => $environment->get('catalogIBlock'),
+		'SECTION_ID' => $arSecItem['ID']
+	);
+
+	$rsElements = \CIBlockElement::GetList(
+		$arElemSort,
+		$arElemFilter,
+		false,
+		false,
+		$arElemSelect
+	);
+
+	$arBrandIds = array();
+	while($arElemItem = $rsElements->Fetch())
+	{
+		if($arElemItem['PROPERTY_BRAND_VALUE'])
+		{
+			$arBrandIds[] = $arElemItem['PROPERTY_BRAND_VALUE'];
+		}
+	}
+	$arBrandIds = array_unique($arBrandIds);
+
+	$arBrandSort = array();
+	$arBrandSelect = array(
+		'ID',
+		'NAME',
+		'CODE'
+	);
+
+	$arBrandFilter = array(
+		'IBLOCK_ID' => $environment->get('brandIBlock'),
+		'ID'        => $arBrandIds
+	);
+
+	$rsBrands = CIBlockElement::GetList(
+		$arBrandSort,
+		$arBrandFilter,
+		false,
+		false,
+		$arBrandSelect
+	);
+
+	$arBrandCodes = array();
+	while($arBrandItem = $rsBrands->Fetch())
+	{
+		$arBrandCodes[$arBrandItem['NAME']] = $arBrandItem['CODE'];
+	}
+
+	return $arBrandCodes;
+}
+
 $arResult["SPECIALS_BLOCK"] = array();
 $arResult["SPECIALS_BLOCK"]["HTML"] = "";
 $arResult["SPECIALS_BLOCK"]["OPENED"] = null;
@@ -12,69 +103,39 @@ foreach($arResult["ITEMS"] as $key => $arItem)
 	if( $arItem["CODE"] == "HIT")
 	{
 		foreach($arItem["VALUES"] as $k => $ar){
-			$arResult["SPECIALS_BLOCK"]["HTML"] .= 
+			$arResult["SPECIALS_BLOCK"]["HTML"] .=
 				'<div class="specials_'.strtolower($arHitPropertyValues[$k]["XML_ID"]).($ar["DISABLED"] ? ' disabled': '').'">
-					<input type="checkbox" value="'.$ar["HTML_VALUE"].'" name="'.$ar["CONTROL_NAME"].'" 
+					<input type="checkbox" value="'.$ar["HTML_VALUE"].'" name="'.$ar["CONTROL_NAME"].'"
 				id="'.$ar["CONTROL_ID"].'"'.($ar["CHECKED"]?'checked="checked"':'').' onclick="smartFilter.click(this)"'.($ar["DISABLED"]?' disabled':'').' />
 					<label for="'.$ar["CONTROL_ID"].'"><i class="icon"></i><span>'.$ar["VALUE"].'</span></label>
 				</div>';
 		}
-	} elseif($arItem["CODE"]=="IN_STOCK"){
+	} elseif($arItem["CODE"] == "IN_STOCK"){
 		sort($arResult["ITEMS"][$key]["VALUES"]);
 		if($arResult["ITEMS"][$key]["VALUES"])
 			$arResult["ITEMS"][$key]["VALUES"][0]["VALUE"]=$arItem["NAME"];
-	} elseif($arItem["CODE"]=="CML2_MANUFACTURER"){
+	} elseif($arItem["CODE"] == "CML2_MANUFACTURER"){
 
-		global $APPLICATION;
+		$arBrandCodes = getBrandsInDir();
 
-		$environment = \Your\Environment\EnvironmentManager::getInstance();
+		$arResult['ITEMS'][$key]['VALUES'] = array();
 
-		$curDir = $APPLICATION->GetCurDir();
-
-		$arUrl = array_unique(explode('/', $curDir));
-		array_splice($arUrl, sizeof($arUrl)-1);
-
-		$arBrandNames = array();
-		foreach($arItem['VALUES'] as &$arValue)
+		foreach($arBrandCodes as $strBrandName => $arBrandCode)
 		{
-			$arBrandNames[] = $arValue['VALUE'];
-		}
+			$curDir = $APPLICATION->GetCurDir();
 
-		$arBrandSort = array();
-		$arBrandSelect = array(
-			'ID',
-			'NAME',
-			'CODE'
-		);
+			$arUrl = array_unique(explode('/', $curDir));
+			array_splice($arUrl, sizeof($arUrl)-1);
 
-		$arBrandFilter = array(
-			'IBLOCK_ID' => $environment->get('brandIBlock'),
-			'NAME' => $arBrandNames
-		);
-
-		$rsBrandElements = CIBlockElement::GetList(
-			$arBrandSort,
-			$arBrandFilter,
-			false,
-			false,
-			$arSelect
-		);
-
-		$arBrandCodes = array();
-		while($arBrandItem = $rsBrandElements->GetNext())
-		{
-			$arBrandCodes[$arBrandItem['NAME']] = $arBrandItem['CODE'];
-		}
-
-		foreach($arItem['VALUES'] as &$arValue)
-		{
 			$arBrandUrl   = $arUrl;
-			$arBrandUrl[] = $arBrandCodes[$arValue['VALUE']];
+			$arBrandUrl[] = $arBrandCode;
 			$arBrandUrl[] = '';
-			
-			$arValue['BRAND_URL'] = implode('/', $arBrandUrl);
+
+			$arResult['ITEMS'][$key]['VALUES'][] = array(
+				'BRAND_URL' => implode('/', $arBrandUrl),
+				'VALUE'     => $strBrandName
+			);
 		}
-		unset($arValue);
 	}
 }
 ?>
